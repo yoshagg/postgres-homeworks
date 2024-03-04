15aff5388be274011a9dbc08aa1496c8f2911c40
yoshagg <smertinkirill@gmail.com>
2024-03-04 21:46:58 +0300

    get started

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
--- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+customer_id int PRIMARY KEY,
+company_name varchar(225) NOT NULL,
+contact_name varchar(255) NOT NULL
+);
+
+CREATE TABLE employees
+(
+employee_id int PRIMARY KEY,
+first_name varchar(30) NOT NULL,
+last_name varchar(30) NOT NULL,
+title varchar(255) NOT NULL,
+birth_date date NOT NULL,
+notes text
+);
+
+CREATE TABLE orders
+(
+order_id int PRIMARY KEY,
+customer_id int REFERENCES customers(customer_id),
+employee_id int REFERENCES employees(employee_id),
+order_date date NOT NULL,
+ship_city varchar(50) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="armchair"
+)
+
+with conn.cursor() as cur:
+    customers = './north_data/customers_data.csv'
+    employees = './north_data/employees_data.csv'
+    orders = './north_data/orders_data.csv'
+    with open(customers, encoding="windows-1251") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            cur.execute(f"""INSERT INTO customers VALUES({row["customer_id"]}, {row["company_name"]}, {row["contact_name"]})""")
+    with open(employees, encoding="windows-1251") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            cur.execute(f"""INSERT INTO employees VALUES({row["employee_id"]}, {row["first_name"]}, {row["last_name"]}, {row["title"]}, {row["birth_date"]}, {row["notes"]})""")
+    with open(orders, encoding="windows-1251") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            cur.execute(f"""INSERT INTO orders VALUES({row["order_id"]},{row["customer_id"]}, {row["employee_id"]}, {row["order_date"]}, {row["ship_city"]})""")
+    rows = cur.fetchall()
